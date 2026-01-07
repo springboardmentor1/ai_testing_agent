68e55faadb5971732584b0145015ccc5026d5b88
sibanisrip <pernisibanisri@gmail.com>
2026-01-07 19:08:08 +0530

    initial commit

---
+++ app/server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app=Flask(
+    __name__,
+    static_folder="../static",
+    static_url_path=""
+)
+@app.route("/")
+def home():
+    return app.send_static_file("index.html")
