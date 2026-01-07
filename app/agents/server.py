229e8ebafa8708fa992818bc3b6b00938997c020
SayanRoy1811 <sayan1811.roy@gmail.com>
2026-01-07 16:37:39 +0530

    Add files via upload
    This is my first commit

---
+++ app/agents/server.py
@@ -0,0 +1,7 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder="../../static")
+
+@app.route("/")
+def home():
+    return send_from_directory(app.static_folder, "index.html")
