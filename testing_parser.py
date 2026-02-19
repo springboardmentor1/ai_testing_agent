8c37d2516e27a9abe305f635f19a21b3b258aa01
sibanisrip <pernisibanisri@gmail.com>
2026-02-19 21:07:58 +0530

    milestone 4 completed

--- testing_parser.py
+++
@@ -1,7 +0,0 @@
-from app.agents.baseline_agent import agent
-
-if __name__ == "__main__":
-    result = agent.invoke({
-        "input": "Open test page and enter name and click submit and verify success"
-    })
-    print(result)
